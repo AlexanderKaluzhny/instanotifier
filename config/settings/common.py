bd12e98135d5fc66d0d93059a5f034be9a3cf31d
Alexander Kaluzhny <alexander.kaluzhny@gmail.com>
2017-02-10 14:54:16 +0200

    updated celery to be 4.0.2

--- config/settings/common.py
+++ config/settings/common.py
@@ -262,13 +262,11 @@ AUTOSLUG_SLUGIFY_FUNCTION = 'slugify.slugify'
 
 ########## CELERY
 INSTALLED_APPS += ('instanotifier.taskapp.celery.CeleryConfig',)
-# if you are not using the django database broker (e.g. rabbitmq, redis, memcached), you can remove the next line.
-INSTALLED_APPS += ('kombu.transport.django',)
-BROKER_URL = env('CELERY_BROKER_URL', default='django://')
-if BROKER_URL == 'django://':
+CELERY_BROKER_URL = env('CELERY_BROKER_URL', default='redis://localhost:6379/0')
+if CELERY_BROKER_URL == 'django://':
     CELERY_RESULT_BACKEND = 'redis://'
 else:
-    CELERY_RESULT_BACKEND = BROKER_URL
+    CELERY_RESULT_BACKEND = CELERY_BROKER_URL
 ########## END CELERY
 
 
--- instanotifier/taskapp/celery.py
+++ instanotifier/taskapp/celery.py
@@ -1,15 +1,12 @@
-
 from __future__ import absolute_import
 import os
 from celery import Celery
 from django.apps import apps, AppConfig
 from django.conf import settings
 
-
 if not settings.configured:
     # set the default Django settings module for the 'celery' program.
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')  # pragma: no cover
-
 
 app = Celery('instanotifier')
 
@@ -21,13 +18,9 @@ class CeleryConfig(AppConfig):
     def ready(self):
         # Using a string here means the worker will not have to
         # pickle the object when using Windows.
-        app.config_from_object('django.conf:settings')
+        app.config_from_object('django.conf:settings', namespace='CELERY')
         installed_apps = [app_config.name for app_config in apps.get_app_configs()]
         app.autodiscover_tasks(lambda: installed_apps, force=True)
-
-        
-
-        
 
 
 @app.task(bind=True)

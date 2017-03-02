6f8e12fa4edd93f68962af5d5c3794523cddcf96
Alexander Kaluzhny <alexander.kaluzhny@gmail.com>
2017-03-02 13:29:18 +0200

    celery_beat_tick patch and the applying script corrected.

--- utility/apply_celery_patch.sh
+++ utility/apply_celery_patch.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+# manual patching of celery beat module
+
+# should be run with the virtualenv activated
+
+# the specific file location is needed for python patch module
+# the file should be placed into the venv directory.
 
 PATCH_DIR="../requirements"
-PATCH_APPLYING_DIR_RELATIVE_PATH="../../"
+
+if [ -z $1 ]
+then
+    PATCH_APPLYING_DIR_RELATIVE_PATH="../../venv"
+else
+    PATCH_APPLYING_DIR_RELATIVE_PATH=$1
+fi
 
 cd $PATCH_DIR
 cp celery_beat_tick.patch $PATCH_APPLYING_DIR_RELATIVE_PATH

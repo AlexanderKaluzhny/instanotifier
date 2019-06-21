881ea9573f44ad43437438511a6f46e1fe7a78a9
Alexander Kaluzhny <alexander.kaluzhny@gmail.com>
2019-06-21 14:57:49 +0300

    mailhog and git files added.

---
+++ instanotifier/fetcher/main.py
@@ -0,0 +1,21 @@
+from celery import shared_task
+from instanotifier.fetcher.rss.fetcher import RssFetcher, fetch_rss_feed
+
+
+@shared_task
+def fetch(url, source_id=None):
+    # Fetcher <-> Source Settings Model
+    # Fetcher < - JSON - > Parser
+
+    # TODO: Determining that a feed is password-protected
+    # TODO: if source_id:
+    #   specify the Etag and Last-Modified values.
+    # * update source_entry_status when the permanently deleted status received
+
+    result = fetch_rss_feed(url)
+
+    # TODO: check feed.status and consider the permanent redirect
+    # TODO: FeedParser object has a lot of unnecessary fields, do we need to pass them
+    # to parser?
+
+    return result
---
+++ instanotifier/parser/main.py
@@ -0,0 +1,17 @@
+from celery import shared_task
+from instanotifier.taskapp.celery import LogErrorsTask
+
+from instanotifier.parser.rss.parser import RssParser
+from instanotifier.notification.views import create_rssnotification_instances
+
+
+@shared_task(base=LogErrorsTask)
+def parse(feed):
+    parser = RssParser(feed)
+    feed_info, feed_items = parser.parse()
+
+    saved_pks = create_rssnotification_instances(feed_items)
+
+    # TODO: take some general info from feed_info and update the FeedSource
+
+    return saved_pks
